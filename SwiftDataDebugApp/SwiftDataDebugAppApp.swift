155d31f059163fdef4d4e00b4732ab942c587238
Stef Kors <stef.kors@gmail.com>
2024-03-04 14:08:35 +0100

    Initial Commit

---
+++ SwiftDataDebugApp/SwiftDataDebugAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftDataDebugAppApp.swift
+//  SwiftDataDebugApp
+//
+//  Created by Stef Kors on 04/03/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftDataDebugAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
